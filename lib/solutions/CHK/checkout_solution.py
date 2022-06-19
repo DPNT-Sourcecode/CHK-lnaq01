a465577eee876842d62c2cee3f97955b1c9fc4b7
sbx_user1051 <sbx_user1051@169.254.177.197>
2022-06-19 16:49:25 +0000

    Sun Jun 19 16:49:25 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,23 +1,19 @@
-
-
-
 sku_to_price_lookup_dict = {
-    "A" : 50,
-    "B" : 30,
-    "C" : 20,
-    "D" : 15,
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15,
 }
 
-# sku_and_num_of_items_to_special_offer_function_lookup_dict = {
-#     "A": 50,
-#     "B": 30,
-#     "C": 20,
-#     "D": 15,
-# }
 
 def calculate_special_offer_amount(sku: str, num_items: int) -> int:
     sub_total = 0
+    # Currently, we are only aware of 2 special offers
+    # 3As for 130, and 2Bs for 45
+    # Lets do this naively to begin with, and we'll change it in future to accomodate other offers
+
     return sub_total
+
 
 # noinspection PyUnusedLocal
 # skus = unicode string
@@ -27,13 +23,5 @@ def checkout(skus: str) -> int:
     # Therefore, we will need to deploy an incorrect solution to find out what this is
     # ______ We have a price table given to us in the txt file, and we should think of a good way to store this
     # ______ We also have some special offers, and we'll have to think of a good way of storing this also
-"""+------+-------+----------------+
-| Item | Price | Special offers |
-+------+-------+----------------+
-| A    | 50    | 3A for 130     |
-| B    | 30    | 2B for 45      |
-| C    | 20    |                |
-| D    | 15    |                |
-+------+-------+----------------+"""
-
     return 0
+
