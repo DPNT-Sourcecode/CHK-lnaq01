17348acd67a42bbac2f8e74c85db5020ebe2cdc9
sbx_user1051 <sbx_user1051@169.254.177.197>
2022-06-19 16:55:25 +0000

    Sun Jun 19 16:55:25 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,7 +6,7 @@ sku_to_price_lookup_dict = {
 }
 
 
-def calculate_amount(sku: str, num_items: int) -> int:
+def calculate_item_amount(sku: str, num_items: int) -> int:
     sub_total = 0
     # Currently, we are only aware of 2 special offers
     # 3As for 130, and 2Bs for 45
@@ -19,17 +19,31 @@ def calculate_amount(sku: str, num_items: int) -> int:
         while num_items >= 2:
             sub_total += 45
             num_items -= 2
+    while num_items > 0:
+        sub_total += sku_to_price_lookup_dict[sku]
+        num_items -= 1
     return sub_total
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+def parse_skus_string(skus: str) -> dict[str, int]:
+    # Outputs dictionary containing the skus, and number of them which occur in the file
+    for sku in skus:
+        
+
+    pass
+
+
 def checkout(skus: str) -> int:
     # ______ At the moment, we do not know the format of the string "skus"
     # e.g. "AAABBBABACGER", "A,B,C,DDD,E" etc.
     # Therefore, we will need to deploy an incorrect solution to find out what this is
     # ______ We have a price table given to us in the txt file, and we should think of a good way to store this
     # ______ We also have some special offers, and we'll have to think of a good way of storing this also
+    sku_num_dict = parse_skus_string(skus)
+
     return 0
+
 
 
