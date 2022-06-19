b6ead914b6b0dec7b86e08ed944bcaf3ac369550
sbx_user1051 <sbx_user1051@169.254.177.197>
2022-06-19 16:19:27 +0000

    Sun Jun 19 16:19:27 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -10,16 +10,19 @@ class TestSum:
 
     def test_sum_float(self):
         assert sum_solution.compute(1.3, 2.7) == 4.0
-
-    def test_sum_float_two(self):
-        assert sum_solution.compute(1.3555, 2.7777) == 4.1332
+        assert sum_solution.compute(1.3555, 2.7777) != 4.1332 # Shows floating point error
 
     def test_sum_string(self):
         assert sum_solution.compute("Hello", " World") == "Hello World"
+        assert sum_solution.compute("Hello", "") == "Hello"
+        assert sum_solution.compute("", " World") == " World"
+        assert sum_solution.compute("", "") == ""
 
     def test_sum_none(self):
         assert sum_solution.compute(None, None) is None
         assert sum_solution.compute(None, 1) is None
+        assert sum_solution.compute(1, None) is None
+
 
 
 
