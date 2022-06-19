f44c330db7f1e27ddeab6d891845c77c5811e690
sbx_user1051 <sbx_user1051@169.254.177.197>
2022-06-19 16:43:25 +0000

    Sun Jun 19 16:43:25 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,7 +3,13 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str) -> int:
-    # At the moment, we do not know the format of the string "skus"
-    # e.g. "AAABBBABACGER", "A,B,C,DDD,E"
-    raise NotImplementedError()
+    # ______ At the moment, we do not know the format of the string "skus"
+    # e.g. "AAABBBABACGER", "A,B,C,DDD,E" etc.
+    # Therefore, we will need to deploy an incorrect solution to find out what this is
+    # ______ We have a price table given to us in the txt file, and we should think of a good way to store this
+    # ______ We also have some special offers, and we'll have to think of a good way of storing this also
+
+
+    return 0
+
 
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,5 +3,6 @@ from lib.solutions.CHK import checkout_solution
 
 class TestSum:
     def test_sum_int(self):
-        assert checkout_solution.checkout() == ""
+        assert checkout_solution.checkout("") == ""
+
 
