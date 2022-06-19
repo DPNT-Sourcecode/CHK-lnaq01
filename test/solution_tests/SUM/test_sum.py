dbb2a481e3336c2d783430e23e022d6f1d906174
sbx_user1051 <sbx_user1051@169.254.177.197>
2022-06-19 16:16:50 +0000

    Sun Jun 19 16:16:50 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,7 +1,7 @@
 from lib.solutions.SUM import sum_solution
 
 
-class TestSum():
+class TestSum:
     def test_sum_int(self):
         assert sum_solution.compute(1, 2) == 3
 
@@ -11,11 +11,15 @@ class TestSum():
     def test_sum_float(self):
         assert sum_solution.compute(1.3, 2.7) == 4.0
 
+    def test_sum_float_two(self):
+        assert sum_solution.compute(1.3555, 2.7777) == 4.1332
+
     def test_sum_string(self):
         assert sum_solution.compute("Hello", " World") == "Hello World"
 
     def test_sum_none(self):
         assert sum_solution.compute(None, None) is None
         assert sum_solution.compute(None, 1) is None
+
 
 
