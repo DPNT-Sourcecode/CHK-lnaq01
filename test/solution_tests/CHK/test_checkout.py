4f8344239d683e3de77e2d4176901053800a9673
sbx_user1051 <sbx_user1051@169.254.177.197>
2022-06-19 17:19:34 +0000

    Sun Jun 19 17:19:34 UTC 2022

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -11,7 +11,8 @@ class TestSum:
         assert checkout_solution.checkout("D") == 15
         assert checkout_solution.checkout("a") == -1
         assert checkout_solution.checkout("-") == -1
-        assert checkout_solution.checkout("ABCa") == -1 # We are unsure if this is a valid sku string or not
+        assert checkout_solution.checkout("ABCa") == -1  # We are unsure if this is a valid sku string or not, we'll
+        # find out after we've deployed it again
         assert checkout_solution.checkout("AxA") == -1
         assert checkout_solution.checkout("ABCD") == 115
         assert checkout_solution.checkout("A") == 50
