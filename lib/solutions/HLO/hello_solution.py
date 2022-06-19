6c08d2a252cf2bcfa402143ccaa55dce4a00a915
sbx_user1051 <sbx_user1051@169.254.177.197>
2022-06-19 16:31:40 +0000

    Sun Jun 19 16:31:40 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -7,5 +7,7 @@ def hello(friend_name: str) -> str:
     # I'm going to run "deploy" intentionally, to get a penalty and feedback
     # All we know at this point is we need to return a string containing a message
     # ___ I have now run "deploy" and seen that I need to return "Hello, World!"
-    return "Hello, World!"
+    # R2 now complete for this challenge
+    return "Hello, "+friend_name+"!"
+
 
--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -3,4 +3,6 @@ from lib.solutions.HLO import hello_solution
 
 class TestSum:
     def test_sum_int(self):
-        assert hello_solution.hello("ignore") == "Hello There"
+        assert hello_solution.hello("ignore") == "Hello, ignore!"
+        assert hello_solution.hello("John") == "Hello, John!"
+
