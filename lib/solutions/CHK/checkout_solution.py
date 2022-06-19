b03a60c9c9dce60a8c22bbac29e69c81d818970b
sbx_user1051 <sbx_user1051@169.254.177.197>
2022-06-19 16:40:25 +0000

    Sun Jun 19 16:40:25 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,8 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
+def checkout(skus: str) -> int:
+    # At the moment, we do not know the format of the string "skus"
+    # e.g. "AAABBBABACGER", "A,B,C,DDD,E"
     raise NotImplementedError()
+
