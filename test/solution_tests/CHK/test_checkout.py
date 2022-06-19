c39a5bfd35127523df560c31a539363333a6d41d
sbx_user1051 <sbx_user1051@169.254.177.197>
2022-06-19 17:10:01 +0000

    Sun Jun 19 17:10:01 UTC 2022

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -11,6 +11,25 @@ class TestSum:
         assert checkout_solution.checkout("D") == 15
         assert checkout_solution.checkout("a") == -1
         assert checkout_solution.checkout("-") == -1
+        assert checkout_solution.checkout("ABCa") == -1 # We are unsure if this is a valid sku string or not
+        assert checkout_solution.checkout("AxA") == -1
+        assert checkout_solution.checkout("ABCD") == -1
+        assert checkout_solution.checkout("A") == -1
+        assert checkout_solution.checkout("AA") == -1
+        assert checkout_solution.checkout("AAA") == -1
+        assert checkout_solution.checkout("AAAA") == -1
+        assert checkout_solution.checkout("AAAAA") == -1
+        assert checkout_solution.checkout("AAAAAA") == -1
+        assert checkout_solution.checkout("B") == -1
+        assert checkout_solution.checkout("BB") == -1
+        assert checkout_solution.checkout("BBB") == -1
+        assert checkout_solution.checkout("BBBB") == -1
+        assert checkout_solution.checkout("ABCDABCD") == -1
+        assert checkout_solution.checkout("BABDDCAC") == -1
+        assert checkout_solution.checkout("AAABB") == -1
+        assert checkout_solution.checkout("ABCDCBAABCABBAAA") == -1
+
+
 
 
 
