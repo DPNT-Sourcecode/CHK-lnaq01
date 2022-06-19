17813b7c6317b71e79f5b49e46fac3830a7e9a05
sbx_user1051 <sbx_user1051@169.254.177.197>
2022-06-19 16:13:50 +0000

    Sun Jun 19 16:13:50 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,6 +1,8 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
     #Could be floats, ints, NaNs, Lists need to check for this
-    #Naive
+    if x is None or y is None:
+        return None
     return x+y
+
 
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,15 +1,21 @@
 from lib.solutions.SUM import sum_solution
 
-class TestSum():
-    def test_sum(self):
-        assert sum_solution.compute(1, 2) == 3
 
+class TestSum():
     def test_sum_int(self):
         assert sum_solution.compute(1, 2) == 3
+
+    def test_sum_zero(self):
+        assert sum_solution.compute(0, 0) == 0
 
     def test_sum_float(self):
-        assert sum_solution.compute(1, 2) == 3
+        assert sum_solution.compute(1.3, 2.7) == 4.0
 
-    def test_sum_int(self):
-        assert sum_solution.compute(1, 2) == 3
+    def test_sum_string(self):
+        assert sum_solution.compute("Hello", " World") == "Hello World"
+
+    def test_sum_none(self):
+        assert sum_solution.compute(None, None) is None
+        assert sum_solution.compute(None, 1) is None
+
 
