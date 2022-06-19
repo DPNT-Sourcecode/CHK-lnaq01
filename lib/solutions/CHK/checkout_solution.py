2fda2befebf0d32de5a05b4feff894e9184b621d
sbx_user1051 <sbx_user1051@169.254.177.197>
2022-06-19 17:04:01 +0000

    Sun Jun 19 17:04:01 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -25,9 +25,6 @@ def calculate_item_amount(sku: str, num_items: int) -> int:
     return sub_total
 
 
-# noinspection PyUnusedLocal
-# skus = unicode string
-
 def parse_skus_string(skus: str) -> dict:
     # Outputs dictionary containing the skus, and number of them which occur in the file
     sku_num_items_dict = {}
@@ -35,18 +32,23 @@ def parse_skus_string(skus: str) -> dict:
     return sku_num_items_dict
 
 
+# noinspection PyUnusedLocal
+# skus = unicode string
 def checkout(skus: str) -> int:
     # ______ At the moment, we do not know the format of the string "skus"
     # e.g. "AAABBBABACGER", "A,B,C,DDD,E" etc.
     # Therefore, we will need to deploy an incorrect solution to find out what this is
     # ______ We have a price table given to us in the txt file, and we should think of a good way to store this
     # ______ We also have some special offers, and we'll have to think of a good way of storing this also
-
+    # We don't know the format, so lets get an incorrect answer on purpose
+    if skus is None:
+        return -1
     sku_num_dict = parse_skus_string(skus)
     basket_total = 0
     for sku, num_items in sku_num_dict.items():
         basket_total += calculate_item_amount(sku, num_items)
     return basket_total
+
 
 
 
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,7 +3,11 @@ from lib.solutions.CHK import checkout_solution
 
 class TestSum:
     def test_sum_int(self):
+        assert checkout_solution.checkout(None) == -1
         assert checkout_solution.checkout("") == 0
         assert checkout_solution.checkout("A") == 50
+        assert checkout_solution.checkout("B") == 30
+        assert checkout_solution.checkout("C") == 20
+        assert checkout_solution.checkout("D") == 15
 
 
