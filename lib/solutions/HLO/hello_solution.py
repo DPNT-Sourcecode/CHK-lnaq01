9d1746a1e164fe6a3bc2d355f8210f12bef19c0b
sbx_user1051 <sbx_user1051@169.254.177.197>
2022-06-19 16:25:57 +0000

    Sun Jun 19 16:25:57 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,5 +2,7 @@
 
 # noinspection PyUnusedLocal
 # friend_name = unicode string
-def hello(friend_name):
-    raise NotImplementedError()
+def hello(friend_name: str) -> str:
+    # Need feedback from "User" Server, before I can continue with implementation
+    return "Hello There"
+
--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -3,4 +3,5 @@ from lib.solutions.HLO import hello_solution
 
 class TestSum:
     def test_sum_int(self):
-        assert hello_solution.compute()
+        assert hello_solution.hello("ignore") == "Hello There"
+
